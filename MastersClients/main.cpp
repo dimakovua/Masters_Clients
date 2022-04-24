ae9fde9c4b86185353c9d9eac68eceaab8eecf21
Dmytro Kovalenko <dimitri.yu.kovalenko@gmail.com>
2022-04-24 17:05:49 +0300

    Initial Commit

---
+++ MastersClients/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MastersClients
+//
+//  Created by Dmytro Kovalenko on 24.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
