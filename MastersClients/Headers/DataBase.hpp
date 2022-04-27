74df459bff0c74defdfe48280ec9c32dc032c6c7
Dmytro Kovalenko <dimitri.yu.kovalenko@gmail.com>
2022-04-27 14:31:41 +0300

    DataBase getters in cpp

--- MastersClients/Headers/DataBase.hpp
+++ MastersClients/Headers/DataBase.hpp
@@ -15,24 +15,12 @@
 class DataBase{
 public:
     DataBase();
-    std::shared_ptr<DataKeeper<Client>> Get_client_db(){
-        return client_db;
-    }
-    std::shared_ptr<DataKeeper<Service>> Get_service_db(){
-        return serv_db;
-    }
-    std::shared_ptr<DataKeeper<Ownership>> Get_ownership_db(){
-        return own_db;
-    }
-    std::shared_ptr<DataKeeper<Contract>> Get_contract_db(){
-        return contr_db;
-    }
-    std::shared_ptr<DataKeeper<Device>> Get_device_db(){
-        return device_db;
-    }
-    std::shared_ptr<DataKeeper<Master>> Get_master_db(){
-        return master_db;
-    }
+    std::shared_ptr<DataKeeper<Client>> Get_client_db();
+    std::shared_ptr<DataKeeper<Service>> Get_service_db();
+    std::shared_ptr<DataKeeper<Ownership>> Get_ownership_db();
+    std::shared_ptr<DataKeeper<Contract>> Get_contract_db();
+    std::shared_ptr<DataKeeper<Device>> Get_device_db();
+    std::shared_ptr<DataKeeper<Master>> Get_master_db();
 private:
     std::shared_ptr<DataKeeper<Client>> client_db;//
     std::shared_ptr<DataKeeper<Service>> serv_db;//
--- MastersClients/Source/DataBase.cpp
+++ MastersClients/Source/DataBase.cpp
@@ -15,3 +15,22 @@ DataBase::DataBase(){
     device_db = std::make_shared<DataKeeper<Device>>();
     master_db = std::make_shared<DataKeeper<Master>>();
 }
+
+std::shared_ptr<DataKeeper<Client>> DataBase::Get_client_db(){
+    return client_db;
+}
+std::shared_ptr<DataKeeper<Service>> DataBase::Get_service_db(){
+    return serv_db;
+}
+std::shared_ptr<DataKeeper<Ownership>> DataBase::Get_ownership_db(){
+    return own_db;
+}
+std::shared_ptr<DataKeeper<Contract>> DataBase::Get_contract_db(){
+    return contr_db;
+}
+std::shared_ptr<DataKeeper<Device>> DataBase::Get_device_db(){
+    return device_db;
+}
+std::shared_ptr<DataKeeper<Master>> DataBase::Get_master_db(){
+    return master_db;
+}
